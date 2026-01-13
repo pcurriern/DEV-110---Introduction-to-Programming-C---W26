1cebb75afbc8fe833fcf7884a3a845d80a065e8d
Parker C <parker.currier@bellevuecollege.edu>
2026-01-12 19:11:14 -0800

    Update Program.cs from working copy

--- modules/week-01-hello-github/starter/Program.cs
+++ modules/week-01-hello-github/starter/Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // TODO: Write code that outputs "Hello, GitHub!" to the console
+        Console.WriteLine("Hello, GitHub!");
     }
 }
